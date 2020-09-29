8f6840370fd3e4cf43467e3a789b3eda81c9344d
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2020-09-29 08:40:39 -0700

    fix client and server

--- client.c
+++ client.c
@@ -1,4 +1,106 @@
-int main() {
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <time.h>
+
+#define TRUE 1
+#define FALSE 0
+
+unsigned int receive_line(int socket_d, char *dist_buffer);
+unsigned int send_message(int socket_d, char *message);
+
+int main(int argc, char **argv) {
+
+    printf("ECHO CLIENT START\n");
+
+    char *ip = (char *) malloc(20);
+    strcpy(ip, "127.0.0.1");
+    unsigned int port = 8080;
+
+    if (argc < 2) {
+        printf("SERVER ADDRESS: 127.0.0.1:8080\n");
+    } else {
+        strcpy(ip, argv[1]);
+        port = atoi(argv[2]);
+    }
+
+    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (server_socket == -1) {
+        perror("ERROR: socket()");
+        exit(-1);
+    }
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 1;
+
+    if (setsockopt(server_socket, SOL_SOCKET, SO_RCVTIMEO, (const char*) &timeout, sizeof(timeout)) == -1) {
+        perror("ERROR: timeout recv");
+        exit(-1);
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = PF_INET;
+    inet_aton(ip, &(server_addr.sin_addr));
+    server_addr.sin_port = htons(port);
+    memset(&(server_addr.sin_zero), 0, sizeof(server_addr.sin_zero));
+
+    socklen_t socklen = sizeof(struct sockaddr);
+
+    if (connect(server_socket, (struct sockaddr*) &server_addr, socklen) == -1) {
+        perror("ERROR: connect()");
+        exit(-1);
+    }
+
+    int is_execute = TRUE;
+    while (is_execute) {
+        char *buffer = (char *) malloc(1000);
+        receive_line(server_socket, buffer);
+        printf("%s", buffer);
+        char *input = (char *) malloc(1000);
+        gets(input);
+
+        if (strncmp(input, "quit", 4) == 0) {
+            is_execute = FALSE;
+        }
+        send_message(server_socket, strcat(input, "\n\r"));
+        free(input);
+    }
+
+    shutdown(server_socket, SHUT_RDWR);
+    close(server_socket);
 }
 
+unsigned int receive_line(int socket_d, char *dist_buffer) {
+    char *ptr = dist_buffer;
+    unsigned int start_size = sizeof(dist_buffer);
+    unsigned int count_bytes = 0;
+    while (recv(socket_d, ptr, 1, 0) != -1) {
+        count_bytes++;
+        ptr++;
+        if (count_bytes == start_size - 2) {
+            start_size += (start_size / 2);
+            dist_buffer = realloc(dist_buffer, start_size);
+        }
+    }
+    return strlen(dist_buffer);
+}
+
+unsigned int send_message(int socket_d, char *message) {
+    char *ptr = message;
+    int all_size = strlen(message);
+    while (all_size > 0) {
+        int send_size = send(socket_d, message, all_size, 0);
+        if (send_size == -1) {
+            return 0;
+        }
+        all_size -= send_size;
+        ptr += send_size;
+    }
+    return 1;
+}
--- server.c
+++ server.c
@@ -1,93 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #define TRUE 1
 #define FALSE 0
 
-void fatal(char *message);
-void* ec_malloc(unsigned int size);
-int prepare_socket_server(unsigned int port);
-int get_connect(int server_socket);
-unsigned int send_message(int client_socket, char *message);
-unsigned int receive_line(int client_socket, char *dist_buffer);
+int get_server_socket(unsigned int port);
+int get_client_socket(int socket_d);
+unsigned int send_message(int socket_d, char *message);
+unsigned int receive_line(int socket_d, char *dist_buffer);
 
 int main(int argc, char **argv) {
+
+    printf("Start Echo Server\n");
+
     unsigned int port = 8080;
 
-    if (argc >= 2) {
+    if (argc < 2) {
+        printf("PORT: 8080\n");
+    } else {
         port = atoi(argv[1]);
+        printf("PORT: %u", port);
     }
 
-    int server_socket = prepare_socket_server(port);
+    int server_socket = get_server_socket(port);
 
     if (listen(server_socket, 5) == -1) {
-        fatal("listen()");
+        perror("ERROR: listen()");
+        exit(-1);
     }
 
     int execute = TRUE;
     while (execute) {
-        int client_socket = get_connect(server_socket);
+        int client_socket = get_client_socket(server_socket);
         send_message(client_socket, "Hello!\n");
-        send_message(client_socket, "This is ECHO SERVER.\n");
-        int cont_receive = FALSE;
+        send_message(client_socket, "This is Echo Server.\n");
+        unsigned int receive = FALSE;
         do {
             send_message(client_socket, "-> ");
-            char *buffer = (char*) ec_malloc(10);
-            cont_receive = receive_line(client_socket, buffer);
+            char *buffer = (char*) malloc(500);
+            receive = receive_line(client_socket, buffer);
+
             printf("RECEIVE: %s\n", buffer);
-            send_message(client_socket, strcat(buffer, "\n"));
-            printf("SENDING: %s\n", buffer);
+
             if (strncmp(buffer, "quit", 4) == 0) {
                 execute = FALSE;
             }
+
             if (strncmp(buffer, "disc", 4) == 0) {
-                cont_receive = FALSE;
+                receive = FALSE;
             }
+            buffer = strcat(buffer, "\n");
+            char *output = (char*) malloc(strlen(buffer) + 10);
+            output = strcpy(output, "ECHO: ");
+            send_message(client_socket, strcat(output, buffer));
             free(buffer);
-        } while(cont_receive && execute);
+            free(output);
+        } while (receive && execute);
+        shutdown(client_socket, SHUT_RDWR);
+        close(client_socket);
     }
 
-    return 0;
+    shutdown(server_socket, SHUT_RDWR);
+    close(server_socket);
 }
 
-int prepare_socket_server(unsigned int port) {
-    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        fatal("socket() create socket");
+int get_server_socket(unsigned int port) {
+    int socket_d = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (socket_d == -1) {
+        perror("ERROR: socket()");
+        exit(-1);
     }
 
     int is_true = TRUE;
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &is_true, sizeof(is_true)) == -1) {
-        fatal("setsockopt() setting TRUE SO_REUSEADDR");
+
+    if (setsockopt(socket_d, SOL_SOCKET, SO_REUSEADDR, &is_true, sizeof(is_true)) == -1) {
+        perror("ERROR: setsockopt()");
+        exit(-1);
     }
 
     struct sockaddr_in server_addr;
-
-    server_addr.sin_family = PF_INET;
+    server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = 0;
     server_addr.sin_port = htons(port);
-    memset(&(server_addr.sin_zero), '\0', sizeof(server_addr.sin_zero));
+    memset(&(server_addr.sin_zero), 0, sizeof(server_addr.sin_zero));
 
-    if (bind(server_socket, (struct sockaddr*) &server_addr, sizeof(struct sockaddr)) == -1) {
-        fatal("bind() setting sockaddr");
+    if (bind(socket_d, (struct sockaddr*) &server_addr, sizeof(struct sockaddr)) == - 1) {
+        perror("ERROR: bind()");
+        exit(-1);
     }
 
-    printf("ECHO SERVER\n");
-    printf("BIND ADDRESS: %s:%d\n\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
-
-    return server_socket;
+    return socket_d;
 }
 
-int get_connect(int server_socket) {
+int get_client_socket(int socket_d) {
     struct sockaddr_in client_addr;
-    socklen_t sin_size = sizeof(struct sockaddr);
-    int client_socket = accept(server_socket, (struct sockaddr*) &client_addr, &sin_size);
+    socklen_t s_len = sizeof(struct sockaddr);
+    int client_socket = accept(socket_d, (struct sockaddr*) &client_addr, &s_len);
     if (client_socket == -1) {
-        fatal("accept() getting connect");
+        perror("ERROR: accept");
+        exit(-1);
     }
 
     printf("NEW CONNECT: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
@@ -95,65 +112,44 @@ int get_connect(int server_socket) {
     return client_socket;
 }
 
-unsigned int send_message(int client_socket, char *message) {
-    int size = strlen(message);
-    while (size > 0) {
-        int sending_bytes_count = send(client_socket, message, size, 0);
-        if (sending_bytes_count == -1) {
+unsigned int send_message(int socket_d, char *message) {
+    unsigned int all_bytes = strlen(message);
+    char *ptr = message;
+    while (all_bytes > 0) {
+        int send_bytes = send(socket_d, message, all_bytes, 0);
+        if (send_bytes == -1) {
+            printf("SEND BYTES: %d", send_bytes);
             return 0;
         }
-        size -= sending_bytes_count;
-        message += sending_bytes_count;
+        all_bytes -= send_bytes;
+        ptr += send_bytes;
     }
     return 1;
 }
 
-unsigned int receive_line(int client_socket, char *dist_buffer) {
-    unsigned int size = sizeof(dist_buffer);
-
-    unsigned int count_bytes = 0;
+unsigned int receive_line(int socket_d, char *dist_buffer) {
+    int end_chars_count = 0;
     char *ptr = dist_buffer;
-    int count_chars = 0;
-    while (recv(client_socket, ptr, 1, 0) != -1) {
+    unsigned int start_size = sizeof(dist_buffer);
+    int count_size = 0;
+    while (recv(socket_d, ptr, 1, 0) != -1) {
+        count_size++;
+
         if (*ptr == '\n' || *ptr == '\r') {
-            count_chars++;
-            if (count_chars == 2) {
+            end_chars_count++;
+            if (end_chars_count == 2) {
                 *(ptr - 1) = '\0';
-                return strlen(dist_buffer);
+                return strlen(dist_buffer) + 2;
             }
         } else {
-            count_chars = 0;
+            end_chars_count = 0;
         }
-        count_bytes++;
         ptr++;
-        if (count_bytes == size - 2) {
-            size += count_bytes / 2;
-            ptr = realloc(dist_buffer, size);
-            ptr += count_bytes;
+
+        if (count_size == start_size - 2) {
+            start_size += (start_size / 2);
+            dist_buffer = realloc(dist_buffer, start_size);
         }
     }
     return 0;
-}
-
-void fatal(char *message) {
-    char *error_message = (char*) malloc(strlen(message) + 50);
-    if (error_message == NULL) {
-        perror("[!!] ERROR: fatal() malloc");
-        exit(-1);
-    }
-
-    strcpy(error_message, "[!!] ERROR: ");
-    strcat(error_message, message);
-
-    perror(error_message);
-
-    exit(-1);
-}
-
-void* ec_malloc(unsigned int size) {
-    void *ptr = malloc(size);
-    if (ptr == NULL) {
-        fatal("ec_malloc() memory allocate");
-    }
-    return ptr;
 }
